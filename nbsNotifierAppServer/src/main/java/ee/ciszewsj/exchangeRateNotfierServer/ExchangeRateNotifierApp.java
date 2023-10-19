84297b321c1ce3d929f48659bf4c0a3f203ff20a
jciszewski <jciszewski@amusys.pl>
2023-10-19 19:11:20 +0200

    Initzialized application

---
+++ nbsNotifierAppServer/src/main/java/ee/ciszewsj/exchangeRateNotfierServer/ExchangeRateNotifierApp.java
@@ -0,0 +1,13 @@
+package ee.ciszewsj.exchangeRateNotfierServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateNotifierApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateNotifierApp.class, args);
+	}
+
+}
---
+++ nbsNotifierAppServer/src/test/java/ee/ciszewsj/exchangeRateNotfierServer/ExchangeRateNotifierTest.java
@@ -0,0 +1,13 @@
+package ee.ciszewsj.exchangeRateNotfierServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateNotifierTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
